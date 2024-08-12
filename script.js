d2069b056463bb29f04d1fc902753a339b991db0
PedroDelgo <pedrodelg.go@gmail.com>
2024-08-12 10:42:55 -0300

    Updating the js file and introducing the "submeter" function

--- script.js
+++ script.js
@@ -1,11 +1,9 @@
-let elementoNome = document.getElementById("nome").value;
-let botaoSubmeter = document.getElementById("botaoSubmeter");
+let botaoSubmeter = document.getElementById("botaoSubmeter"); //criando um variavel pra tag html botao
 
-botaoSubmeter.addEventListener("click",submeter);
+botaoSubmeter.addEventListener("click",submeter); //criando um evento ao clicar no botao tem outros comandos possiveis de fazer 
 
-console.log(elementoNome);
-
-function submeter (){
-    alert("função submeter");
+function submeter (){ //função que vai ser usado quando o evento acontecer no caso ao clicar o botao
+    let elementoNome = document.getElementById("nome").value; //aqui ele ira colocar o valor submetido nessa variavel o input esta la no html
+    console.log(elementoNome); //o console ira printar o nome inserido
 }
 
