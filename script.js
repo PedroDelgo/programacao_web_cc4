25643a6618034edf4ca946cb0dabfdf7b0286627
PedroDelgo <pedrodelg.go@gmail.com>
2024-08-19 09:31:56 -0300

    feat: introduces email validation

--- script.js
+++ script.js
@@ -5,10 +5,16 @@ botaoSubmeter.addEventListener("click",submeter); //criando um evento ao clicar 
 function submeter (){ //função que vai ser usado quando o evento acontecer no caso ao clicar o botao
     let elementoNome = document.getElementById("nome").value; //aqui ele ira colocar o valor submetido nessa variavel o input esta la no html
     let elementoIdade = document.getElementById("idade").value;
-    let elementoCPF = document.getElementById("cpf").value;
+    let elementoEmail = document.getElementById("email").value;
     console.log(elementoNome); //o console ira printar o nome inserido
     console.log(elementoIdade);
-    console.log(elementoCPF);
+    console.log(elementoEmail);
+    console.log(emailValidation(elementoEmail));
 }
 
 // TO DO coloar validação de emial no codigo so aceitar email validos
+
+function emailValidation(elementoEmail){
+    return /^\w+(\.\w+)*@\w+(\.\w+)+$/.test(elementoEmail) ;
+}
+
